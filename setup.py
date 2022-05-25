7179b2ae8ec632b16172d770dc2d7b0ede3c269e
neiltthomas <106176109+neiltthomas@users.noreply.github.com>
2022-05-25 16:13:00 -0600

    Upgrading to use class

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 import os
+from files import files
 
-CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
-PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
-PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
-TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
-LABEL_MAP_NAME = 'label_map.pbtxt'
+# CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
+# PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
+# PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
+# TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
+# LABEL_MAP_NAME = 'label_map.pbtxt'
 
-paths = {
-    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
-    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
-    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
-    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
-    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
-    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
-    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
- }
+# paths = {
+#     'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+#     'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
+#     'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+#     'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+#     'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+#     'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+#     'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
+#     'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+#     'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+#     'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+#     'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+#     'PROTOC_PATH':os.path.join('Tensorflow','protoc')
+#  }
 
-for path in paths.values():
+
+
+for path in files.paths.values():
     if not os.path.exists(path):
         if os.name == 'posix':
             os.makedirs(path)
@@ -29,9 +32,9 @@ for path in paths.values():
             print("Error")
 
 pwd = os.getcwd()
-m_path = os.path.join(pwd, paths['APIMODEL_PATH'])
+m_path = os.path.join(pwd, files.paths['APIMODEL_PATH'])
 
-if not os.path.exists(os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection')):
+if not os.path.exists(os.path.join(files.paths['APIMODEL_PATH'], 'research', 'object_detection')):
     os.system(f'git clone https://github.com/tensorflow/models {m_path}')
 
 if os.name=='posix':  
@@ -40,4 +43,4 @@ if os.name=='posix':
 
 print('------------SETUP FINISHED----------------')
 
-VERIFICATION_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'builders', 'model_builder_tf2_test.py')
+VERIFICATION_SCRIPT = os.path.join(files.paths['APIMODEL_PATH'], 'research', 'object_detection', 'builders', 'model_builder_tf2_test.py')
