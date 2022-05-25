d77b8f06a99b97f6819eb494b2feb204abb85297
noelfranthomas <noelfranthomas@gmail.com>
2022-05-25 13:25:19 -0600

    Create label map

--- create_label_map.py
+++ create_label_map.py
@@ -21,23 +21,17 @@ paths = {
     'PROTOC_PATH':os.path.join('Tensorflow','protoc')
  }
 
-for path in paths.values():
-    if not os.path.exists(path):
-        if os.name == 'posix':
-            os.makedirs(path)
-        else:
-            print("Error")
+files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
 
-pwd = os.getcwd()
-m_path = os.path.join(pwd, paths['APIMODEL_PATH'])
+labels = [{'name':'licence', 'id':1}]
 
-if not os.path.exists(os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection')):
-    os.system(f'git clone https://github.com/tensorflow/models {m_path}')
-
-if os.name=='posix':  
-    os.system('brew install protobuf')
-    os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install . ')
-
-print('------------SETUP FINISHED----------------')
-
-VERIFICATION_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'builders', 'model_builder_tf2_test.py')
+with open(files['LABELMAP'], 'w') as f:
+    for label in labels:
+        f.write('item { \n')
+        f.write('\tname:\'{}\'\n'.format(label['name']))
+        f.write('\tid:{}\n'.format(label['id']))
+        f.write('}\n')
