8af9d32a1d332627bf4fbb5822d09c70d48bcd97
noelfranthomas <noelfranthomas@gmail.com>
2022-05-25 18:51:18 -0600

    Create TF record

--- create_TF_records.py
+++ create_TF_records.py
@@ -1,42 +1,17 @@
 import os
+from files import files
 
-CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
-PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
-PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
-TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
-LABEL_MAP_NAME = 'label_map.pbtxt'
+SCRIPTS_PATH = files.paths['SCRIPTS_PATH']
 
-paths = {
-    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
-    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
-    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
-    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
-    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
-    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
-    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
- }
-
-files = {
-    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
-    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
-}
-
-SCRIPTS_PATH = paths['SCRIPTS_PATH']
-
-if not os.path.exists(files['TF_RECORD_SCRIPT']):
+if not os.path.exists(files.files['TF_RECORD_SCRIPT']):
     os.system(f'git clone https://github.com/nicknochnack/GenerateTFRecord {SCRIPTS_PATH}')
 
-TF_RECORD_SCRIPT = files['TF_RECORD_SCRIPT']
-IMAGE_PATH_train = os.path.join(paths['IMAGE_PATH'], 'train')
-IMAGE_PATH_test = os.path.join(paths['IMAGE_PATH'], 'test')
-LABELMAP = files['LABELMAP']
-ANNOTATION_PATH_train = os.path.join(paths['ANNOTATION_PATH'], 'train.record')
+TF_RECORD_SCRIPT = files.files['TF_RECORD_SCRIPT']
+IMAGE_PATH_train = os.path.join(files.paths['IMAGE_PATH'], 'train')
+IMAGE_PATH_test = os.path.join(files.paths['IMAGE_PATH'], 'test')
+LABELMAP = files.files['LABELMAP']
+ANNOTATION_PATH_train = os.path.join(files.paths['ANNOTATION_PATH'], 'train.record')
+ANNOTATION_PATH_test = os.path.join(files.paths['ANNOTATION_PATH'], 'test.record')
 
-f'python {TF_RECORD_SCRIPT} -x {IMAGE_PATH_train} -l {LABELMAP} -o {os.path.join(paths['ANNOTATION_PATH'], 'train.record')} 
-!python {files['TF_RECORD_SCRIPT']} -x {IMAGE_PATH_test} -l {LABELMAP} -o {os.path.join(paths['ANNOTATION_PATH'], 'test.record')} 
+os.system(f'python {TF_RECORD_SCRIPT} -x {IMAGE_PATH_train} -l {LABELMAP} -o {ANNOTATION_PATH_train}')
+os.system(f'python {TF_RECORD_SCRIPT} -x {IMAGE_PATH_test} -l {LABELMAP} -o {ANNOTATION_PATH_test}')
