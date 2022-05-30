0bad350fe536e6818d45d8bba1128773c74b6b2c
noelfranthomas <noelfranthomas@gmail.com>
2022-05-30 14:48:18 -0600

    Added Real-time functionality

--- detection.py
+++ detection.py
@@ -6,19 +6,16 @@ from object_detection.builders import model_builder
 from object_detection.utils import config_util
 from files import files
 
-@tf.function
 def detect_fn(image):
     # Load pipeline config and build a detection model
     configs = config_util.get_configs_from_pipeline_file(files.files['PIPELINE_CONFIG'])
     detection_model = model_builder.build(model_config=configs['model'], is_training=False)
 
-    # Restore checkpoint
-    ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-    ckpt_path = os.path.join(files.paths['CHECKPOINT_PATH'], 'ckpt-11')
-    ckpt.restore(ckpt_path).expect_partial()
+    # Create a Checkpoint that will manage two objects with trackable state,
+    checkpoint = tf.compat.v2.train.Checkpoint(model=detection_model)
+    status = checkpoint.restore(os.path.join(files.paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
 
     image, shapes = detection_model.preprocess(image)
     prediction_dict = detection_model.predict(image, shapes)
     detections = detection_model.postprocess(prediction_dict, shapes)
     return detections
-
---
+++ predict_with_image.py
@@ -0,0 +1,53 @@
+from files import files
+from detection import detect_fn
+import os
+import tensorflow as tf
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg') # Enables GUI
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+
+
+IMAGE_TO_USE = os.path.join('test', 'Cars432.png')
+
+category_index = label_map_util.create_category_index_from_labelmap(files.files['LABELMAP'])
+
+IMAGE_PATH = os.path.join(files.paths['IMAGE_PATH'], IMAGE_TO_USE)
+
+img = cv2.imread(IMAGE_PATH)
+image_np = np.array(img)
+
+input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+detections = detect_fn(input_tensor)
+
+num_detections = int(detections.pop('num_detections'))
+detections = {key: value[0, :num_detections].numpy()
+              for key, value in detections.items()}
+detections['num_detections'] = num_detections
+
+# detection_classes should be ints.
+detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+label_id_offset = 1
+image_np_with_detections = image_np.copy()
+
+viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=5,
+            min_score_thresh=.8,
+            agnostic_mode=False)
+
+print(detections['detection_scores'])
+
+plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
+plt.show()
